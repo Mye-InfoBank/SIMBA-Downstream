d813e0f378f8039c0b412dc0724a30848c327e44
Nico Trummer <nictru32@gmail.com>
2024-03-17 12:09:48 +0000

    Properly implement annotation export functionality

--- export.py
+++ export.py
@@ -17,16 +17,27 @@ def get_cell_labels():
     regex = re.compile(r".*-cell-labels-[A-Z0-9]+.csv")
     return get_files(regex)
 
+def format_download_id(original: str):
+    return os.path.basename(original).replace('-', '_')[:-4]
+
 
 @module.ui
 def export_ui():
     return ui.input_action_button("update", "Update"), \
-        ui.navset_tab(
-            ui.nav_panel("Gene sets",
-                ui.output_ui("render_gene_sets")
+        ui.layout_columns(
+            ui.card(
+                    ui.card_header("Gene sets"),
+                    ui.output_ui("render_gene_sets"),
+                    ui.card_footer(
+                        ui.download_button("download_gene_set", "Download")
+                    )
             ),
-            ui.nav_panel("Cell labels",
-                ui.output_ui("render_cell_labels")
+            ui.card(
+                    ui.card_header("Cell labels"),
+                    ui.output_ui("render_cell_labels"),
+                    ui.card_footer(
+                        ui.download_button("download_cell_labels", "Download")
+                    )
             )
         )
 
@@ -50,10 +61,25 @@ def export_server(input, output, session):
     def render_gene_sets():
         gene_sets = _gene_sets.get()
 
-        return [ui.input_action_button(f"download_{os.path.basename(f).replace('-', '_')[:-4]}", f"Download {os.path.basename(f)}") for f in gene_sets]
+        return ui.input_select("gene_set", "Select file", choices={f: os.path.basename(f) for f in gene_sets})
 
     @render.ui
     def render_cell_labels():
         cell_labels = _cell_labels.get()
 
-        return [ui.input_action_button(f"download_{os.path.basename(f).replace('-', '_')[:-4]}", f"Download {os.path.basename(f)}") for f in cell_labels]
+        return ui.input_select("cell_labels", "Select file", choices={f: os.path.basename(f) for f in cell_labels})
+    
+    @render.download(
+            filename=lambda: os.path.basename(input["gene_set"].get())
+    )
+    def download_gene_set():
+        file_path = input["gene_set"].get()
+        print(file_path)
+        return file_path
+    
+    @render.download(
+            filename=lambda: os.path.basename(input["cell_labels"].get())
+    )
+    def download_cell_labels():
+        file_path = input["cell_labels"].get()
+        return file_path
