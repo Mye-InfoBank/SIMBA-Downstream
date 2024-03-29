4b7ec9b9aacabd8eb225d2247ad32db91b14718f
Nico Trummer <nictru32@gmail.com>
2024-03-29 08:35:00 +0000

    Remove sudo from installation

--- requirements.sh
+++ requirements.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get update -y && sudo apt-get install -y python3-pip python3-dev && \
+apt-get update -y && apt-get install -y python3-pip python3-dev && \
     python3 -m pip install --upgrade pip && \
     python3 -m pip install --user -r requirements.txt && \
     Rscript requirements.R
