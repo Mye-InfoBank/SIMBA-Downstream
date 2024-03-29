1bbbcf6ba539dc48be9572e13caced55975c2aea
Nico Trummer <nictru32@gmail.com>
2024-03-29 09:14:11 +0000

    Fix packages not in PATH bug

--- requirements.sh
+++ requirements.sh
@@ -2,5 +2,5 @@
 
 apt-get update -y && apt-get install -y python3-pip python3-dev && \
     python3 -m pip install --upgrade pip && \
-    python3 -m pip install --user -r requirements.txt && \
+    python3 -m pip install -r requirements.txt && \
     Rscript requirements.R
