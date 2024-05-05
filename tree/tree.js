9e073e4e9a3569ffbdaaa30483dbb85f0474be9a
Nico Trummer <nictru32@gmail.com>
2024-05-05 16:07:36 +0000

    Add particles

--- tree/tree.js
+++ tree/tree.js
@@ -9,7 +9,12 @@ if (Shiny) {
                 .dagMode("td")
                 .dagLevelDistance(200)
                 .backgroundColor('#101020')
-            
+                .linkColor(() => 'rgba(255,255,255,0.2)')
+                .nodeOpacity(0.9)
+                .linkDirectionalParticles(2)
+                .linkDirectionalParticleWidth(0.8)
+                .linkDirectionalParticleSpeed(0.006)
+
             graph(el).graphData(payload);
         }
     }
