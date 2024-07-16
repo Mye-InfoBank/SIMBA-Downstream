2a2a849c4ad5571f9b14d0091f3a232ab1ee464e
Nico Trummer <nictru32@gmail.com>
2024-07-16 09:47:16 +0000

    Fix crash if data/annotations does not exist

--- export.py
+++ export.py
@@ -4,14 +4,19 @@ import re
 
 annotations_directory = "data/annotations"
 
+
 def get_files(regex, directory=annotations_directory):
-    return [os.path.join(directory, f) 
-             for f in os.listdir(directory) 
-             if regex.match(f)]
+    if not os.path.exists(directory):
+        return []
+    return [os.path.join(directory, f)
+            for f in os.listdir(directory)
+            if regex.match(f)]
+
 
 def get_gene_sets():
     regex = re.compile(r".*-gene-sets-[A-Z0-9]+.csv")
     return get_files(regex)
+
 
 def get_cell_labels():
     regex = re.compile(r".*-cell-labels-[A-Z0-9]+.csv")
@@ -23,20 +28,20 @@ def export_ui():
     return ui.input_action_button("update", "Update"), \
         ui.layout_columns(
             ui.card(
-                    ui.card_header("Gene sets"),
-                    ui.output_ui("render_gene_sets"),
-                    ui.card_footer(
-                        ui.download_button("download_gene_set", "Download")
-                    )
+                ui.card_header("Gene sets"),
+                ui.output_ui("render_gene_sets"),
+                ui.card_footer(
+                    ui.download_button("download_gene_set", "Download")
+                )
             ),
             ui.card(
-                    ui.card_header("Cell labels"),
-                    ui.output_ui("render_cell_labels"),
-                    ui.card_footer(
-                        ui.download_button("download_cell_labels", "Download")
-                    )
+                ui.card_header("Cell labels"),
+                ui.output_ui("render_cell_labels"),
+                ui.card_footer(
+                    ui.download_button("download_cell_labels", "Download")
+                )
             )
-        )
+    )
 
 
 @module.server
@@ -53,7 +58,6 @@ def export_server(input, output, session):
         files = get_cell_labels()
         _cell_labels.set(files)
 
-
     @render.ui
     def render_gene_sets():
         gene_sets = _gene_sets.get()
@@ -65,17 +69,17 @@ def export_server(input, output, session):
         cell_labels = _cell_labels.get()
 
         return ui.input_select("cell_labels", "Select file", choices={f: os.path.basename(f) for f in cell_labels})
-    
+
     @render.download(
-            filename=lambda: os.path.basename(input["gene_set"].get())
+        filename=lambda: os.path.basename(input["gene_set"].get())
     )
     def download_gene_set():
         file_path = input["gene_set"].get()
         print(file_path)
         return file_path
-    
+
     @render.download(
-            filename=lambda: os.path.basename(input["cell_labels"].get())
+        filename=lambda: os.path.basename(input["cell_labels"].get())
     )
     def download_cell_labels():
         file_path = input["cell_labels"].get()
